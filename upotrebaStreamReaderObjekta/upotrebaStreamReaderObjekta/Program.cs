a037f1dc1857209733ca96c26b43e1f3db5dca37
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2021-03-30 18:28:33 +0200

    Add files via upload

---
+++ upotrebaStreamReaderObjekta/upotrebaStreamReaderObjekta/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+namespace CitanjeTeksta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"D:\My Documents\NOOP\datoteka.txt");
+            // Čitamo datoteku liniju po liniju
+            while (!sr.EndOfStream)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+            // Zatvaramo datoteku
+            sr.Close();
+            Console.ReadKey();
+        }
+    }
+}
